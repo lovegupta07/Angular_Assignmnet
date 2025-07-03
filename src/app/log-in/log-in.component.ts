ddfbd987cc69b8340988a310606ab6c28f416306
Love Kumar <lovek@pronnel.com>
2025-07-04 02:19:30 +0530

    modify login page for better responsiveness

--- src/app/log-in/log-in.component.ts
+++ src/app/log-in/log-in.component.ts
@@ -5,19 +5,19 @@ import { Component } from '@angular/core';
   templateUrl: './log-in.component.html',
   styleUrls: ['./log-in.component.css']
 })
+
 export class LoginComponent {
   email: string = '';
   password: string = '';
   showPassword: boolean = false;
 
   get emailInvalid(): boolean {
-  return this.email !== '' && !/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(this.email);
-}
+    return this.email !== '' && !/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(this.email);
+  }
 
-get passwordInvalid(): boolean {
-  return this.password !== '' && this.password.trim() === '';
-}
-
+  get passwordInvalid(): boolean {
+    return this.password !== '' && this.password.trim() === '';
+  }
 
   togglePassword(): void {
     this.showPassword = !this.showPassword;
